d876f590eaedc5960ef9a7544b7775d22728b96d
InformedGuy <184560653+InformedGuy@users.noreply.github.com>
2025-10-31 08:19:58 +0300

    Add operate function

--- calculator.js
+++ calculator.js
@@ -1,15 +1,41 @@
-function add(firstNumber, secondNumber) {
-    return firstNumber + secondNumber;
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+function operate(x, symbol, y) {
+    let result = 0;
+    switch(symbol) {
+        case "+":
+            result = add(x, y);
+            break;
+        case "-":
+            result = subtract(x, y);
+            break;
+        case "*":
+            result = multiply(x, y);
+            break;
+        case "/":
+            result = divide(x, y);
+            break;
+        default:
+            alert("Invalid symbol!");
+    }
+
+    return result;
 }
 
-function subtract(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
+function add(first, second) {
+    return first + second;
 }
 
-function multiply(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
+function subtract(first, second) {
+    return first - second;
 }
 
-function divide(firstNumber, secondNumber) {
-    return firstNumber / secondNumber;
+function multiply(first, second) {
+    return first * second;
+}
+
+function divide(first, second) {
+    return first / second;
 }
