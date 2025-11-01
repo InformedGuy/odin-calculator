23c58f3db92c4b4975a3d9570e0aab17144c9947
InformedGuy <184560653+InformedGuy@users.noreply.github.com>
2025-11-01 15:47:34 +0300

    Add calculator functionality
    Create functionality to populate the display, get the result & clear the display

--- calculator.js
+++ calculator.js
@@ -1,19 +1,16 @@
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = "";
-
 function operate(x, symbol, y) {
+    let intX = parseInt(x);
+    let intY = parseInt(y);
+
     switch(symbol) {
         case "+":
-            return add(x, y);
+            return add(intX, intY);
         case "-":
-            return subtract(x, y);
+            return subtract(intX, intY);
         case "*":
-            return multiply(x, y);
-        case "/":
-            return divide(x, y);
+            return multiply(intX, intY);
         default:
-            alert("Invalid symbol!");
+            return divide(intX, intY);
     }
 }
 
@@ -31,4 +28,39 @@ function multiply(first, second) {
 
 function divide(first, second) {
     return first / second;
+}
+
+const numberButtons = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const display = document.getElementById("display");
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        display.value += event.target.textContent;
+    });
+});
+
+operators.forEach(operator => {
+    operator.addEventListener("click", (event) => {
+        display.value += ` ${event.target.textContent} `;
+    });
+});
+
+const result = document.getElementById("result");
+result.addEventListener("click", getResult);
+
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", clearDisplay);
+
+function getResult() {
+    let userInput = display.value.split(" ");
+    if (userInput.length === 3) {
+        display.value = operate(...userInput);
+    } else {
+        alert("Invalid arguments!");
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
 }
